d9f5288579f79ebec9bdc301136296a010973982
Santosh Prajapati <101245775+Prajapati-santosh@users.noreply.github.com>
2024-08-19 05:00:29 +0000

    commit

--- auth/index.js
+++ auth/index.js
@@ -1,1 +1,13 @@
+import express from "express";
 
+
+const app=express();
+
+app.get("/",(req,res)=>{
+    res.send("Hello");
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log(process.env.USERNAME);
+    console.log(`listening on ${process.env.PORT}`);
+})
